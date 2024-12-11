7c9284444d7e8aec3333df05fc5bc2b2b451ce80
Md Ismail <mdismaeelkhan345@gmail.com>
2024-12-11 15:19:14 +0530

    updated local host

--- NaishCaresE-Commers/src/Redux/store/store.js
+++ NaishCaresE-Commers/src/Redux/store/store.js
@@ -3,19 +3,18 @@ import userReducer from "../../Redux/slices/usersSlice";
 import { persistStore, persistReducer } from "redux-persist";
 import storage from "redux-persist/lib/storage";
 
-// Persist configuration
 const persistConfig = {
     key: "root", // The key used to store the Redux state in localStorage
     storage, // Using localStorage
     whitelist: ["cartItem", "wishList"], // Only persist cart and wishlist data
-    debug: false, // Disable debug in production
+    debug: false, 
 };
 
 const persistedReducer = persistReducer(persistConfig, userReducer);
 
 export const store = configureStore({
     reducer: {
-        Ecommers: persistedReducer, // Add persisted reducer
+        Ecommers: persistedReducer, 
     },
     middleware: (getDefaultMiddleware) =>
         getDefaultMiddleware({
