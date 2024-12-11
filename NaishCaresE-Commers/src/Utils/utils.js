6abced3daefacdebe26cb917286b0df9ab21b203
Md Ismail <mdismaeelkhan345@gmail.com>
2024-12-11 15:23:53 +0530

    fix the error

--- NaishCaresE-Commers/src/Utils/utils.js
+++ NaishCaresE-Commers/src/Utils/utils.js
@@ -1,5 +1,5 @@
-export const LOCALHOST_DOMAIN = "http://localhost:10000/api/v1/user";
-export const BACKEND_DOMAIN = "";
+// export const LOCALHOST_DOMAIN = "http://localhost:10000/api/v1/user";
+// export const BACKEND_DOMAIN = "";
 
 export const requestOptions = {
     headers: {
