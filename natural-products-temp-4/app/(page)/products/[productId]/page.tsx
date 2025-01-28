0bff8dac8c3d68a3ed7a0f83b307012a7e38c721
Md Ismail <mdismaeelkhan345@gmail.com>
2025-01-28 19:16:49 +0530

    updated -2.01

--- natural-products-temp-4/app/(page)/products/[productId]/page.tsx
+++ natural-products-temp-4/app/(page)/products/[productId]/page.tsx
@@ -4,7 +4,7 @@ import ProdDetails from "@/app/Components/ProdDetails/ProdDetails";
 export default async function ProductDetails({ params }: {
   params: { productId: string };
 }) {
-  const productId = await Number(params.productId);
+  const productId = Number(params.productId);
 
   return (
     <>
