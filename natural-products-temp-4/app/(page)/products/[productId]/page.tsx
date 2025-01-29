a5c12bcf3e4e11c9ef8fcb8d6448721707539780
Md Ismail <mdismaeelkhan345@gmail.com>
2025-01-29 12:06:17 +0530

    update 2.04

--- natural-products-temp-4/app/(page)/products/[productId]/page.tsx
+++ natural-products-temp-4/app/(page)/products/[productId]/page.tsx
@@ -1,19 +1,18 @@
 import React from "react";
 import ProdDetails from "@/app/Components/ProdDetails/ProdDetails";
 import { fetchProductDetails } from "@/app/Utils/utils";
+import { product } from "@/app/Type/Type";
 
-export default async function ProductDetails({ params }: {
-  params: { productId: string };
-}) {
+export default async function ProductDetails({ params }: { params: { productId: string } }) {
   const { productId } = params;
 
-  const res = await fetchProductDetails(productId);
-  console.log("res", res);
+  try {
+    const res: product = await fetchProductDetails(productId);
+    console.log("Fetched Product:", res);
 
-
-  return (
-    <>
-      <ProdDetails data={res} />
-    </>
-  );
+    return <ProdDetails data={res} />;
+  } catch (error) {
+    console.error("Error fetching product details:", error);
+    return <p>Failed to load product details.</p>;
+  }
 }
--- natural-products-temp-4/app/Utils/utils.ts
+++ natural-products-temp-4/app/Utils/utils.ts
@@ -1,31 +1,21 @@
+import { product } from "../Type/Type";
+
 export async function fetchProducts(category: string) {
   const url =
     category === "All"
       ? "https://fakestoreapi.com/products"
       : `https://fakestoreapi.com/products/category/${category}`;
 
-  const response = await fetch(url, {
-    next: { revalidate: 3600 }, // Optional: revalidate every hour
-  });
+  const response = await fetch(url);
 
-  if (!response.ok) {
-    throw new Error("Failed to fetch products");
-  }
+  if (!response.ok) throw new Error("Failed to fetch products");
 
   return response.json();
 }
 
-export async function fetchProductDetails(id: string) {
+export async function fetchProductDetails(id: string): Promise<product> {
   const url = `https://fakestoreapi.com/products/${id}`;
-
-  const response = await fetch(url, {
-    next: { revalidate: 3600 }, // Optional: revalidate every hour
-  });
-
-  if (!response.ok) {
-    throw new Error("Failed to fetch products");
-  }
-
+  const response = await fetch(url);
+  if (!response.ok) throw new Error("Failed to fetch products");
   return response.json();
 }
-
