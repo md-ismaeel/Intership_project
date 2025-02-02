95cbaebabbdcdeece86e3bb8008224dcf6656c9e
Md Ismail <mdismaeelkhan345@gmail.com>
2025-02-02 14:15:45 +0530

    update

--- natural-products-temp-4/app/sign-in/[[...sign-in]]/page.tsx
+++ natural-products-temp-4/app/sign-in/[[...sign-in]]/page.tsx
@@ -1,11 +1,23 @@
-import { SignIn } from '@clerk/nextjs'
+"use client";
 
-export default function Page() {
+import { SignIn } from "@clerk/nextjs";
+import { useSearchParams } from "next/navigation";
+import { useMemo } from "react";
+
+const SignInPage = () => {
+    const searchParams = useSearchParams();
+
+    // Get the "redirect_url" from query parameters
+    const redirectUrl = useMemo(() => {
+        const query = searchParams.get("redirect_url");
+        return query || "/";
+    }, [searchParams]);
+
     return (
-        <>
-            <section className='w-full pt-4 flex justify-center items-center'>
-                <SignIn />
-            </section>
-        </>
-    )
-}
+        <section className="w-full pt-4 flex justify-center items-center">
+            <SignIn redirectUrl={redirectUrl} />
+        </section>
+    );
+};
+
+export default SignInPage;
--- natural-products-temp-4/app/sign-up/[[...sign-up]]/page.tsx
+++ natural-products-temp-4/app/sign-up/[[...sign-up]]/page.tsx
@@ -1,11 +1,21 @@
-import { SignUp } from "@clerk/nextjs";
+"use client";
 
-export default function Page() {
+import { SignUp } from '@clerk/nextjs';
+import { useSearchParams } from 'next/navigation';
+import { useMemo } from 'react';
+
+export default function SignUpPage() {
+  const searchParams = useSearchParams();
+
+  // Ensure redirect_url is always a string
+  const redirectUrl = useMemo(() => {
+    const query = searchParams.get('redirect_url');
+    return query || '/sign-in'; // Default to '/sign-up' if no redirect_url
+  }, [searchParams]);
+
   return (
-    <>
-      <section className="w-full pt-4 flex justify-center items-center">
-        <SignUp />
-      </section>
-    </>
+    <section className="w-full pt-4 flex justify-center items-center">
+      <SignUp redirectUrl={redirectUrl} />
+    </section>
   );
 }
